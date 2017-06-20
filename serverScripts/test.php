db1e85944cec38ca7cf6a3988234c0dcc4f56241
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-06-20 11:55:46 +0100

    testowe wysyłanie danych

--- serverScripts/test.php
+++ serverScripts/test.php
@@ -1,5 +1,10 @@
 <?php
 header('Content-type: application/json');
 $arr = array ('a'=>'1','b'=>'2','c'=>'3','d'=>'4','e'=>'5');
+if(isset($_GET["name"]) && $_GET["pswd"]))
+{
+	$arr["name"]=$_GET["name"];
+	$arr["pswd"]=$_GET["pswd"];
+}
 echo $_GET['callback'] . '('.json_encode($arr).')';
 ?>
