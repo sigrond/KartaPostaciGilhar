731fd3e77a11053b933ce808a6c95706d11eb9eb
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-06-20 11:59:33 +0100

    zgubiony isset

--- serverScripts/test.php
+++ serverScripts/test.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: application/json');
 $arr = array ('a'=>'1','b'=>'2','c'=>'3','d'=>'4','e'=>'5');
-if(isset($_GET["name"]) && $_GET["pswd"]))
+if(isset($_GET["name"]) && isset($_GET["pswd"]))
 {
 	$arr["name"]=$_GET["name"];
 	$arr["pswd"]=$_GET["pswd"];
