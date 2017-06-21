cb952d509d443ac8f538b4f1e9e86ad1538bdf6d
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-06-21 12:53:20 +0100

    sprawdzanie hasla i loginu w bazie

--- serverScripts/login.php
+++ serverScripts/login.php
@@ -12,7 +12,27 @@ if(isset($_GET["name"]) && isset($_GET["pswd"]))
 		die("Connection failed: " . $conn->connect_error);
 	}
 	
-	$arr["status"]="OK";
+	$sql = "SELECT pswd FROM gracz WHERE name='"+$_GET["name"]+"'";
+	$result = $conn->query($sql);
+	
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			if($row["pswd"]===$_GET["pswd"])
+			{
+				$arr["status"]="OK";
+			}
+			else
+			{
+				$arr["status"]="WRONGPSWD";
+			}
+		}
+	} else {
+		$arr["status"]="NOSUCHLOGIN";
+	}
+	$conn->close();
+	
+	
 }
 else
 {
