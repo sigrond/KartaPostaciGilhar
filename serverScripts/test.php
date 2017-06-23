6d6d9ad07b109d3be8686b02132c43daa957528f
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-06-23 13:36:55 +0100

    następna próba z crossorigin

--- serverScripts/test.php
+++ serverScripts/test.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-type: application/json');
+header("Access-Control-Allow-Origin: *");
 $arr = array ('a'=>'1','b'=>'2','c'=>'3','d'=>'4','e'=>'5');
 if(isset($_GET["name"]) && isset($_GET["pswd"]))
 {
