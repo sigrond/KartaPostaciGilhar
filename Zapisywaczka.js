cd11de06b9bcf785846b4c636a1adb0f751eb114
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-08-06 18:54:47 +0100

    wczytywanie z bazy

--- Zapisywaczka.js
+++ Zapisywaczka.js
@@ -5,7 +5,7 @@
 fieldColector=new Array();
 
 function zapisz(nazwa, wartosc){
-	localStorage.setItem(String(nazwa), String(wartosc));
+	//localStorage.setItem(String(nazwa), String(wartosc));
 	console.log(String(nazwa)+":"+String(wartosc));
 	//$("#debug_ids"+strona,).append(String(nazwa)+" TEXT, ");
 	//tu moze pojawić się jakiś ajax
@@ -14,7 +14,8 @@ function zapisz(nazwa, wartosc){
 }
 
 function wczytaj(nazwa){
-	return localStorage.getItem(String(nazwa));
+	//return localStorage.getItem(String(nazwa));
+	return fieldColector[strona][String(nazwa)];
 }
 
 function zapiszPostacDoBazy(){
@@ -49,9 +50,49 @@ function zapiszPostacDoBazy(){
 		});
 }
 
+function wczytajPostacZBazy(){
+	$.post({
+			url: 'http://projektgil.cba.pl/KartaPostaciGilhar/serverScripts/wczytajDanePostaci.php',
+			crossDomain: true,
+			dataType: 'json',
+			contentType: "application/x-www-form-urlencoded",//bo czemy niby application/json miało by poprawnie działać z POST jak opisano w standardzie
+			data: {
+				fields: JSON.stringify(fieldColector),
+				name: sessionStorage.uname,
+				pswd: sessionStorage.upswd
+				},
+			success: function (json) {
+				console.log(json);
+				var myStr=String("");
+				$.each(json,function(i,v){
+					console.log(i);
+					myStr+=i+": "+JSON.stringify(v)+", ";
+					if(i==='fields')
+					{
+						console.log(JSON.stringify(v));
+						fieldColector=v;
+						console.log(fieldColector);
+						localStorage.setItem('fieldColector', JSON.stringify(fieldColector));
+						$(myOuter+"input").each(function(){
+							this.value=wczytaj(this.id);//wczytanie wszystkich wartości
+						});
+					}
+				});
+				//console.log(myStr);
+				$("#debug_response1").html(myStr);
+			},
+			error: function (responseData, textStatus, errorThrown) {
+				alert('POST failed.');
+				console.log(responseData);
+				console.log(textStatus);
+				console.log(errorThrown);
+			}
+		});
+}
+
 $(document).ready(function(){
 	var i=0;
-	var myOuter="";
+	myOuter="";
 	if($("#actual_page").length)
 		myOuter="#actual_page ";
 	console.log(myOuter);
@@ -64,6 +105,8 @@ $(document).ready(function(){
 		localStorage["myVersion"]=$("#myVersion").html();
 		console.log("myVersion updated");
 		localStorage["fieldColector"]=null;
+		for(var i_strona=1;i_strona<=13;i_strona++)
+		localStorage['zapisane'+i_strona]=false;
 	}
 	if(fieldColector==null || updated)
 	{
@@ -97,7 +140,7 @@ $(document).ready(function(){
 		});
 		console.log(myObj);
 		console.log("strona: "+strona);
-		$.post({
+		/*$.post({
 			url: 'http://projektgil.cba.pl/KartaPostaciGilhar/serverScripts/zapiszDanePostaci'+strona+'.php',
 			crossDomain: true,
 			dataType: 'json',
@@ -117,7 +160,9 @@ $(document).ready(function(){
 				console.log(textStatus);
 				console.log(errorThrown);
 			}
-		});
+		});*/
+		//zapiszPostacDoBazy();
+		//wczytajPostacZBazy();
 		localStorage['zapisane'+strona]="true";
 	}
 	
---
+++ serverScripts/wczytajDanePostaci.php
@@ -0,0 +1,100 @@
+<?php
+
+header('Content-type: application/json', true);
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+$arr = array ();
+
+//$arr["fields"]=$_POST["fields"];
+
+$myFields=json_decode($_POST["fields"],true);
+
+
+
+$servername = "mysql.cba.pl";
+$username = "sigrond";
+include "configs/vars.php";
+$db_name="sigrond";
+
+
+
+$conn = mysqli_connect($servername, $username, $password);
+mysqli_select_db($conn, $db_name);
+
+if (!$conn) {
+	$arr["dbce1"]="DBCONNECTIONERROR\n";
+	$arr["dbce2"]=mysqli_connect_error();
+}
+
+$sql = "SELECT * FROM gracz WHERE name='".$_POST["name"]."'";
+$arr["mySQLquery0"]=$sql;
+$result = mysqli_query($conn, $sql);
+if ($result === false) {
+	//error
+	$arr["dbq0e1"]="QUERYERROR\n";
+	$arr["dbq0e2"]=mysqli_error($conn);
+}
+else {
+	//success
+	$arr["dbq0s"]= "QUERYSUCCESS";
+}
+$uprawnieniaPrawidlowe=false;
+$id_gracza=0;
+if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			if($row["pswd"]===$_POST["pswd"])
+			{
+				$arr["status"]="OK";
+				$uprawnieniaPrawidlowe=true;
+				$id_gracza=$row["id"];
+			}
+			else
+			{
+				$arr["status"]="WRONGPSWD";
+			}
+		}
+} else {
+	$arr["status"]="NOSUCHLOGIN";
+}
+
+if($uprawnieniaPrawidlowe){
+foreach($myFields as $key => $value){
+
+	$sqlFields="id";
+	//$sqlValues="VALUES( ".$id_gracza;
+	$i=0;
+	foreach($value as $fieldName => $fieldValue){
+		//if($i++<20){
+		$sqlFields.=", ".$fieldName;
+		//$sqlValues.=", '".$fieldValue."'";//}
+	}
+	$sql="SELECT ".$sqlFields." FROM DanePostaci".$key." WHERE id='".$id_gracza."'";
+	//$sql = "INSERT INTO DanePostaci".." (id, myGenericID0, myGenericID4) VALUES(1,'".$_POST["myGenericID0"]."','".$_POST["myGenericID4"]."')";
+	$arr["mySQLquery".$key]=$sql;
+	$result = mysqli_query($conn, $sql);
+
+	if ($result === false) {
+		//error
+		$arr["dbq".$key."e1"]="QUERYERROR\n";
+		$arr["dbq".$key."e2"]=mysqli_error($conn);
+	}
+	else {
+		//success
+		$arr["dbq".$key."s"]= "QUERYSUCCESS";
+	}
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$arr["fields"][$key]=$row;
+		}
+	}
+}
+}
+mysqli_close($conn);
+
+echo json_encode($arr);
+
+?>
